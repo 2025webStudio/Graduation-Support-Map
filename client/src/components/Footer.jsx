a8fc30643e7c7958f8873a0ac0ccb7b692f21fb3
jjeongee <starstar117@naver.com>
2025-12-17 01:53:56 +0900

    chore : navbar, footer에서 스타일은 css로 빼서 정리

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,22 +1,24 @@
+import "../styles/global.css";
+
 export default function Footer() {
     return (
-        <footer style={styles.footer}>
-            <div style={styles.inner}>
+        <footer className="footer">
+            <div className="footer-inner">
                 {/* Left Title */}
-                <div style={styles.title}>
+                <div className="footer-title">
                     <div>Seoul</div>
                     <div>Graduation</div>
                     <div>Exhibition</div>
                 </div>
 
                 {/* School List */}
-                <div style={styles.schools}>
-                    <ul style={styles.ul}>
+                <div className="footer-universities">
+                    <ul>
                         <li>건국대학교</li>
                         <li>고려대학교</li>
                         <li>국민대학교</li>
                     </ul>
-                    <ul style={styles.ul}>
+                    <ul>
                         <li>서울과학기술대학교</li>
                         <li>서울시립대학교</li>
                         <li>홍익대학교</li>
@@ -24,46 +26,10 @@ export default function Footer() {
                 </div>
             </div>
 
-            <div style={styles.bottom}>
+            <div className="footer-bottom">
                 2025 WebStudio
             </div>
         </footer>
     );
 }
 
-const styles = {
-    footer: {
-        background: "#2b2b2b",
-        color: "#ccc",
-    },
-    inner: {
-        maxWidth: "1200px",
-        margin: "0 auto",
-        padding: "64px 16px",
-        display: "flex",
-        justifyContent: "space-between",
-    },
-    title: {
-        fontSize: "36px",
-        fontWeight: 300,
-        color: "#bdbdbd",
-        lineHeight: "1.2",
-    },
-    schools: {
-        display: "flex",
-        gap: "48px",
-        fontSize: "13px",
-    },
-    ul: {
-        listStyle: "none",
-        padding: 0,
-        margin: 0,
-    },
-    bottom: {
-        borderTop: "1px solid #444",
-        textAlign: "center",
-        padding: "16px 0",
-        fontSize: "12px",
-        color: "#777",
-    },
-};
